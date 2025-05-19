cf2226bd81f7eb15cf602229be1e934cc2447d9c
Luis <luisfelipe.pereira37@gmail.com>
2025-05-19 12:41:05 -0300

    SextoCommit

--- obj/Debug/net8.0/TorneioTenisMesa.AssemblyInfo.cs
+++ obj/Debug/net8.0/TorneioTenisMesa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TorneioTenisMesa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a47ea04d6ae459d78fd1c1b3ef82beb95155a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TorneioTenisMesa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TorneioTenisMesa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
